ed02778db03e0d09474e416e4df59ef89fd5bb0f
yzhu <yzhu@anigo.com.au>
2018-06-19 11:14:11 +1000

    Impl installer to register NLog factory.

--- BaseUtil/IoC/IoCLoggerInstaller.cs
+++ BaseUtil/IoC/IoCLoggerInstaller.cs
@@ -4,10 +4,13 @@ namespace com.zhusmelb.Util.IoC
     using Castle.MicroKernel.Registration;
     using Castle.MicroKernel.SubSystems.Configuration;
 
+    using Castle.Core.Logging;
+    using Castle.Services.Logging.NLogIntegration;
+    
     public class IoCLoggerInstaller : IWindsorInstaller
     {
         public void Install(IWindsorContainer container, IConfigurationStore store) {
-
+            container.Register(Component.For<ILoggerFactory>().ImplementedBy<NLogFactory>());
         }
     }
 }
